8e5fc40ece1877c86ab62973a8268d4747f387dd
Vinicius Carli Bustamanti <33128193+ViniciusCarli@users.noreply.github.com>
2018-03-01 11:04:58 -0300

    tava trollando

--- Project1.dpr
+++ Project1.dpr
@@ -2,13 +2,12 @@ program Project1;
 
 uses
   Vcl.Forms,
-  Unit1 in '..\Leitor de CSV\Leitor-de-CSV-Delphi\Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
